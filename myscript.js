b35bfb9e7cc685b63fa4a0890f6bc56268b6f569
Elka Friedman <elkafriedman1@gmail.com>
2022-12-07 18:57:53 -0500

    javascript

---
+++ myscript.js
@@ -0,0 +1,34 @@
+///console.log("I am so super cool!");
+///console.log("My name is Elka");
+
+///const firstName = "elka";
+///const lastName = "friedman";
+
+///console.log(firstName + ' ' + lastName);
+
+///const favColor = 'green', favNumber = 18;
+///console.log(favColor, favNumber)
+///let product; 
+///product = 3 * 5;
+///console.log(product);
+
+///let myString = 'This is not a string'
+///let newString = myString.replace('not','')
+///console.log(myString);
+///console.log(newString);
+///let myNum = 7;
+///let myNewNum = (myNum += 1);
+///console.log(myNewNum);
+
+///let num = 4;
+///if (num > 100){
+///    console.log(`${num} is greater than 100`);
+///} else if (num == 100) {
+///    console.log(`${num} is 100`);
+///    }
+/// else if (num < 100) {
+///    console.log(`${num} is less than 100`);
+///} 
+
+console.log(`ten is less than 20. true or false? ${10<20}`)
+
